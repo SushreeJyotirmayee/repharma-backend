70ac0e33b73be56a12644f1d38c23f6325e3d19e
SushreeJyotirmayee <sjyotirmayeesahoo@gmail.com>
2025-05-26 12:12:54 +0530

    Updated db.js for Railway env

--- db.js
+++ db.js
@@ -2,12 +2,20 @@
 const mysql = require('mysql2');
 
 // ✅ Create connection using Railway's default environment variable names
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT
+// });
+
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: 'gondola.proxy.rlwy.net',
+  user: 'root',
+  password: 'gGzPaJumRGhOjrnNlKUsvEcChccWkvNl',
+  database: 'railway',
+  port: 25707
 });
 
 // ✅ Connect to Railway MySQL
